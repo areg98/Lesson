dbc2649c59e8049c2b11d936cb264636c41195f7
areg98 <areg98>
2022-09-13 17:34:24 +0400

    Operators

---
+++ src/Operators.java
@@ -0,0 +1,33 @@
+public class Operators {
+    public static void  operators(){
+        int a = 10;
+        int b = 23;
+        int result;
+        a += 2;
+        b += 3;
+        result = a++;
+        System.out.println(result);
+        result = ++a;
+        System.out.println(result);
+        result = a--;
+        System.out.println(result);
+        result = --a;
+        System.out.println(result);
+        result = a + b;
+        System.out.println(result);
+        result = a - b;
+        System.out.println(result);
+        result = b * a;
+        System.out.println(result);
+        result = b / a;
+        System.out.println(result);
+        result = b % a;
+        System.out.println(result);
+        result *= 2;
+        System.out.println(result);
+        result /= 2;
+        System.out.println(result);
+        result %= 3;
+        System.out.println(result);
+    }
+}
