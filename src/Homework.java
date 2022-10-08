fa2c0fa3ab6a0030cc361662dfc3889c46b369fa
areg98 <areg98>
2022-10-09 00:44:23 +0400

    homework 08/10

--- src/Homework.java
+++ src/Homework.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Homework {
 
 
@@ -23,18 +25,65 @@ public class Homework {
     static int secondLargest(int[] arr){
         return sort(arr)[1];
     }
+    //https://www.w3resource.com/java-exercises/array/index.php
+    //  18
+    static int secondSmallest(int[] arr){
+        return sort(arr)[arr.length - 2];
+    }
+
+    //Напишите класс, который генерирует серию из 100 случайных однозначных
+    //неотрицательных чисел (цифр).
+    //Класс должен подсчитать, сколько раз в этой серии встречалась каждая
+    //цифра, и вывести на экран соответствующую информацию.
+    static void randomNumbers(int n){
+        Random random = new Random();
+        int[] arr = new int[n];
+        int[] counts = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(0,10);
+            if (i % 100 == 0) System.out.println();
+            System.out.print("\u001B[32m" + arr[i] + " " + "\u001B[0m");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (arr[i] == j) counts[j]++;
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < counts.length; i++) {
+            float tokos = (float) (counts[i] * 100) / n;
+            System.out.println(i + ": " + counts[i] + " " + tokos + "%");
+        }
+    }
+
+    //Напишите метод, который получает в качестве параметра строку и возвращает
+    //строку, составленную только из заглавных букв русского алфавита.
+    //Например, для строки АБвrде 123ЖзИ метод возвратит АБЖИ.
+
+    static String getUpperCases(String word){
+        String result = "";
+        for (int i = 0; i < word.length(); i++) {
+            if ((int) word.charAt(i) >= 1040 && (int) word.charAt(i) <= 1071) result += word.charAt(i);
+        }
+        return result;
+    }
+
+
 
 
 
 
         public static void main (String[]args){
-            int[] arr1 = {5,28,62,8,6};
-            sort(arr1);
-            System.out.print("Sorted Array: ");
-            for (int i = 0; i < arr1.length; i++) {
-                System.out.print(arr1[i] + " ");
-            }
-            System.out.println();
-            System.out.print("The second largest number: " + secondLargest(arr1));
+//            int[] arr1 = {5,28,62,8,6};
+//            sort(arr1);
+//            System.out.print("Sorted Array: ");
+//            for (int i = 0; i < arr1.length; i++) {
+//                System.out.print(arr1[i] + " ");
+//            }
+//            System.out.println();
+//            System.out.print("The second largest number: " + secondLargest(arr1) + "\n");
+//            System.out.print("The second smallest number: " + secondSmallest(arr1) + "\n");
+            randomNumbers(2450);
+//            System.out.println(getUpperCases("АБвrде 123ЖзИ"));
         }
     }
