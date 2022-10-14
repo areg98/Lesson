aa8cac1911fbf09488cce5b59b4e9dde6a4bc044
areg98 <areg98>
2022-10-14 15:00:16 +0400

    Refactor

--- src/ReversArray.java
+++ src/ReversArray.java
@@ -1,19 +1,22 @@
 public class ReversArray {
-    public static void main(String[] args) {
-        int[] arr = new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-        }
-        for (int x : arr) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
+
+    static  public void ReversArray(int [] arr){
         for (int i = 0; i < arr.length / 2; i++) {
             arr[i] = arr[i] + arr[arr.length - i - 1];
             arr[arr.length - i - 1] = arr[i] - arr[arr.length - i - 1];
             arr[i] = arr[i] - arr[arr.length - i - 1];
         }
-        for (int x : arr) {
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = new int[10];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = i + 1;
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
+        ReversArray(myArray);
+        for (int x : myArray) {
             System.out.print(x + " ");
         }
     }
